12aa57e22cc45a59f468cd0379b457339cf93114
p0lluxstar <web-registra@yandex.ru>
2024-04-13 17:49:38 +0300

    init: node-crone, puppeteer-extra; feat: add scrape and file db

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,5 @@
-import puppeteer from 'puppeteer';
+import { product1 } from './scrapes/product1';
+import { product2 } from './scrapes/product2';
 
-(async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
-  const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // Type into search box
-  await page.type('.devsite-search-field', 'automate beyond recorder');
-
-  // Wait and click on first result
-  const searchResultSelector = '.devsite-result-item-link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector('text/Customize and automate');
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-
-  await browser.close();
-})();
+product1();
+product2();
