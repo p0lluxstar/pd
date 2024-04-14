8bdfbe2bb462d3ca0b7413fe60ed81832c74b2f8
p0lluxstar <web-registra@yandex.ru>
2024-04-15 01:17:07 +0300

    refac: scraper; feat: add a second store

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { product1 } from './scrapes/product1';
-import { product2 } from './scrapes/product2';
+import { product1 } from './products/product1';
+import { product2 } from './products/product2';
 
 product1();
 product2();
+
+console.log(`\x1b[35mApplication is currently running!\x1b[0m`);
