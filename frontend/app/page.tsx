f45af9eb535a0e6828811435fcffb849e97f26ee
p0lluxstar <web-registra@yandex.ru>
2024-05-15 00:16:24 +0300

    feat: add arch styles; init: prettier

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
+  let a = 'd';
   return (
     <>
       <h1>Главная</h1>
+      <h2>H2</h2>
+      <img src="https://img3.fonwall.ru/o/yd/art-wallpaper-food.jpeg?auto=compress&fit=resize&w=1200&display=large" />
     </>
   );
 }
