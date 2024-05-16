6837e0f30ac1315c31444cc4660228247a91fe74
p0lluxstar <web-registra@yandex.ru>
2024-05-16 14:16:00 +0300

    refac: setting prettier and eslint

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,7 @@
-export default function Home() {
-  let a = 'd';
+export default function Home(): JSX.Element {
   return (
     <>
-      <h1>Главная</h1>
-      <h2>H2</h2>
-      <img src="https://img3.fonwall.ru/o/yd/art-wallpaper-food.jpeg?auto=compress&fit=resize&w=1200&display=large" />
+      <div className="main">Main</div>
     </>
   );
 }
