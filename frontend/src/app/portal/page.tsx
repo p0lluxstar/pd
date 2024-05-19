2c5e4b8d0c0164c82a4e496ad3c4e8066147a326
p0lluxstar <web-registra@yandex.ru>
2024-05-19 16:55:32 +0300

    feat: add empty portal page

--- frontend/src/app/portal/page.tsx
+++ frontend/src/app/portal/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Portal | PD',
+  description: 'Description portal page',
+};
+
 const Portal = (): JSX.Element => (
   <>
     <h1>Portal</h1>
