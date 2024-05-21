94c432eac4d7a51de9d35483dd54658c00d38080
p0lluxstar <web-registra@yandex.ru>
2024-05-21 11:59:14 +0300

    feat: add fetch in shops page

--- frontend/src/app/portal/shops/page.tsx
+++ frontend/src/app/portal/shops/page.tsx
@@ -1,7 +1,28 @@
+'use client';
+
 export default function ShopsPage(): JSX.Element {
+  const fetchData = async (): Promise<void> => {
+    try {
+      const response = await fetch('http://localhost:3001/api/data');
+
+      const result = await response.json();
+
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h1>Shops</h1>
+      <button
+        onClick={() => {
+          void fetchData();
+        }}
+      >
+        Получить вcе магазины
+      </button>
     </>
   );
 }
