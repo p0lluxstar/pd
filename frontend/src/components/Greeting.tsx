1f8388181487108e62ed8ae23498937e92773635
p0lluxstar <web-registra@yandex.ru>
2025-09-04 01:29:00 +0300

    reat: add mobile styles on greeting page;

--- frontend/src/components/Greeting.tsx
+++ frontend/src/components/Greeting.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import Link from 'next/link';
-import styles from '../styles/components/greeting.module.scss';
+import { useContext } from 'react';
+import { ThemeContext } from '../context/ThemeContextProvider';
+import darkStyles from '../styles/components/greeting/darkGreeting.module.scss';
+import styles from '../styles/components/greeting/greeting.module.scss';
+import lightStyles from '../styles/components/greeting/lightGreeting.module.scss';
+
 import Logo from './Logo';
 
 export default function Greeting(): JSX.Element {
+  const themeContext = useContext(ThemeContext);
+  const themeStyles = themeContext.theme === 'light' ? lightStyles : darkStyles;
+
   return (
     <>
-      <div className={styles.main}>
+      <div className={`${themeStyles.main} ${styles.main}`}>
         <div className={styles.wrapper}>
           <div className={styles.logo}>
             <Logo width={60} height={80} />
